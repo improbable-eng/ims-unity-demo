e4fac734daca43be37e7f93711ac2665adce1643
Marc Harvey-Hill <Marchhill@users.noreply.github.com>
2022-08-09 14:48:52 +0100

    client can list sessions

--- Client/Assets/Scripts/ListSessions.cs
+++ Client/Assets/Scripts/ListSessions.cs
@@ -21,7 +21,7 @@ public class ListSessions : MonoBehaviour
         Refresh();
     }
 
-    public void Refresh()
+    public async void Refresh()
     {
         // Remove all old buttons
         foreach (GameObject button in buttons)
@@ -30,13 +30,23 @@ public class ListSessions : MonoBehaviour
         }
         buttons.Clear();
 
-        // TODO: Get a list of sessions from the session manager to create join buttons for each
-
-        for (int i = 0; i < 5; i++)
+        // Get a list of sessions
+        IMS.V0ListSessionsResponse res;
+        try
         {
-            var name = "myGame";
-            var connected = 5;
-            var maxPlayers = 10;
+            res = await API.sessionManagerApi.ListSessionsV0Async(API.projectId, API.sessionType);
+        }
+        catch (IMS.ApiException e)
+        {
+            Debug.LogError(e);
+            return;
+        }
+
+        foreach (IMS.V0Session session in res.Sessions)
+        {
+            var name = session.Session_status["name"];
+            var connected = Int32.Parse(session.Session_status["connected"]);
+            var maxPlayers = Int32.Parse(session.Session_status["maxPlayers"]);
 
             // Do not display a game that is full
             if (connected == maxPlayers)
@@ -48,8 +58,8 @@ public class ListSessions : MonoBehaviour
             var button = Instantiate(buttonPrefab, contentContainer.transform);
             
             button.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = name + " (" + connected + " / " + maxPlayers + ")";
-            button.GetComponent<JoinGameButton>().ip = "127.0.0.1";
-            button.GetComponent<JoinGameButton>().port = 8000;
+            button.GetComponent<JoinGameButton>().ip = session.Address;
+            button.GetComponent<JoinGameButton>().port = session.Ports.FirstOrDefault().Port;
             button.GetComponent<JoinGameButton>().name = name;
             button.GetComponent<JoinGameButton>().maxPlayers = maxPlayers;
 
