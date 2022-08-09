50aa0308a3d81f20f2bfcbebc867f9e8202fd95d
Marc Harvey-Hill <Marchhill@users.noreply.github.com>
2022-08-09 14:35:29 +0100

    client can create new sessions

--- Client/Assets/Scripts/CreateGame.cs
+++ Client/Assets/Scripts/CreateGame.cs
@@ -19,14 +19,33 @@ public class CreateGame : MonoBehaviour
         public int maxPlayers;
     }
 
-    public void Go()
+    public async void Go()
     {
         OptionsData.name = nameText.GetComponentInChildren<TMPro.TMP_InputField>().text;
         OptionsData.maxPlayers = Int32.Parse(maxPlayersText.GetComponentInChildren<TMPro.TMP_InputField>().text);
 
-        // TODO: Create a session with Zeuz and use the returned IP and port
-        var ip = "127.0.0.1";
-        var port = 8000;
+        IMS.V0CreateSessionResponse res;
+        var req = new IMS.V0CreateSessionRequestBody();
+
+        // Initialise configuration
+        GameConfiguration config = new GameConfiguration();
+        config.name = OptionsData.name;
+        config.maxPlayers = OptionsData.maxPlayers;
+        req.Session_config = JsonUtility.ToJson(config);
+
+        // Create a new session
+        try
+        {
+            res = await API.sessionManagerApi.CreateSessionV0Async(API.projectId, API.sessionType, req);
+        }
+        catch (IMS.ApiException e)
+        {
+            Debug.LogError(e);
+            return;
+        }
+
+        var ip = res.Address;
+        var port = res.Ports.FirstOrDefault().Port;
 
         Debug.Log("Joining " + ip + ":" + port);
         OptionsData.ip = ip;
